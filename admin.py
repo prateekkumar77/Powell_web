2324384a7be434d17a45bbcb6e47b06c7a1312f3
prateekkumar77 <70167383+prateekkumar77@users.noreply.github.com>
2022-08-26 22:17:22 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import reports, patient, employee, lab_equipment
-
-# Register your models here.
-
-admin.site.register(reports)
-admin.site.register(patient)
-admin.site.register(employee)
-admin.site.register(lab_equipment)
